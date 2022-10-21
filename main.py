8fea4702410afb424c9149f23e6c9b441fada47a
Danilo-Danilo-Danilo <danilo.lacerda@aluno.ufca.edu.br>
2022-10-21 04:49:46 -0300

    Spawn de inimigos e principio de tiro

--- main.py
+++ main.py
@@ -1,25 +1,69 @@
 import pygame
-import numpy as np
 
 pygame.init()
 
 win = pygame.display.set_mode((768, 512))
 
-pygame.display.set_caption("NECROLORDE!")
+pygame.display.set_caption("Tower of the Necrolord!")
 
+class Projetil:
 
-class Unidade:
+    def __init__(self, pos, sprite):
+        self.x = pos[0]
+        self.y = pos[1]
+        self.sprite = sprite
+    def draw_projetil(self):
+        win.blit(ata_esq, (self.x, self.y))
 
+    def mover(self):
+        self.x += 1
+
+    def fora_da_tela(self):
+        if self.x > 768:
+            return True
+        elif self.x <= 768:
+            return False
+
+class Inimigo:
     def __init__(self, hp, ataque, sprite):
         self.hp = hp
         self.ataque = ataque
         self.sprite = sprite
+        self.x = None
+        self.y = None
+        self.linha = None
+
+    def spawn(self, linha):
+        self.x = 780
+        self.y = 112 + (64 * linha)
+        self.linha = linha
+        return linha
+
+    def mover(self):
+        if self.x is not None:
+            self.x -= 1
+            win.blit(self.sprite, (self.x, self.y))
+
+
+class Unidade:
+
+    def __init__(self, hp, ataque, sprite, sprite_ataque):
+        self.hp = hp
+        self.ataque = ataque
+        self.sprite = sprite
         self.select = False
+        self.sprite_ataque = sprite_ataque
+        self.cooldown_cont = 0
 
-
+    def cooldown(self):
+        if self.cooldown_cont >= 10:
+            self.cooldown_cont = 0
+        elif self.cooldown_cont > 0:
+            self.cooldown_cont += 1
 class Lugar:
-    def __init__(self, pos):
+    def __init__(self, pos, linha):
         self.x, self.y = pos
+        self.linha = linha
         self.unidade = None
         self.rect = None
         self.surface = None
@@ -31,7 +75,7 @@ class Lugar:
         self.surface = pygame.Surface(self.size)
         self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
 
-    def colocar_unidade(self, event, unit):
+    def colocar_unidade(self, event, unit, inimigos, projeteis):
         """"Coloca unidade selecionada em um espaço"""
         x, y = pygame.mouse.get_pos()
         if event.type == pygame.MOUSEBUTTONDOWN:
@@ -41,13 +85,32 @@ class Lugar:
                         self.unidade = unit
 
                         unit = None
-        self.mostrar_unidade()
+        bala = self.mostrar_unidade(inimigos)
+        if bala is not None:
+            projeteis.append(bala)
+        for bala in projeteis:
+            bala.draw_projetil()
+            bala.mover()
         return unit
 
-    def mostrar_unidade(self):
+    def mostrar_unidade(self, inimigos):
         """Mostra a unidade no espaço"""
+
         if self.unidade is not None:
+            self.unidade.cooldown()
             win.blit(self.unidade.sprite, (self.x + 8, self.y - 16))
+            for i in range(len(inimigos)):
+                if (inimigos[i].linha == self.linha) and (self.unidade.cooldown_cont == 0) :
+                    bala = Projetil((self.x, self.y), self.unidade.sprite_ataque)
+                    self.unidade.cooldown_cont = 1
+                    return bala
+        else:
+            return None
+
+
+
+
+
 
 
 class Tabuleiro:
@@ -60,10 +123,10 @@ class Tabuleiro:
             lugar_linha = []
             for j in range(6):
                 if i == 0 and j == 0:
-                    lugar_linha.append(Lugar(pos_ini))
+                    lugar_linha.append(Lugar(pos_ini, j))
                 else:
                     pos_atual = (pos_ini[0] + (i * 64)), (pos_ini[1] + (j * 64))
-                    lugar_linha.append(Lugar(pos_atual))
+                    lugar_linha.append(Lugar(pos_atual, j))
             self.lugar.append(lugar_linha)
 
 
@@ -111,22 +174,41 @@ run = True
 bg_img = pygame.image.load('background.png')
 card_esq = pygame.image.load('sprites\card-skeleton-001.png')
 un_esq = pygame.image.load('sprites\skeleton-turret-001.png')
+in_mago = pygame.image.load('sprites\mage-001.png')
+ataque_esq = pygame.image.load('sprites\sbrick.png')
+ata_esq = pygame.transform.scale(ataque_esq, (32, 32))
 bg = (pygame.transform.scale(bg_img, (768, 512)))
 carta_1 = [40, 15]
 tam_carta = (64, 64)
 c_esq = pygame.transform.scale(card_esq, tam_carta)
 uni_esq = pygame.transform.scale(un_esq, tam_carta)
+mago = pygame.transform.scale(in_mago, tam_carta)
 unidade_mouse = None
-esqueleto = Unidade(100, 100, uni_esq)
+esqueleto = Unidade(100, 100, uni_esq, ata_esq)
 carta_esqueleto = Carta((7, 7), c_esq, esqueleto, 100)
-lugar1 = Lugar((22, 128))
+maguin = Inimigo(40, 60, mago)
 tabuleiro = Tabuleiro((22, 128))
-
+inimigos = []
+projeteis = []
 while run:
 
     win.blit(bg_img, (0, 0))
 
     for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_1:
+                maguin.spawn(0)
+            if event.key == pygame.K_2:
+                maguin.spawn(1)
+            if event.key == pygame.K_3:
+                maguin.spawn(2)
+            if event.key == pygame.K_4:
+                maguin.spawn(3)
+            if event.key == pygame.K_5:
+                maguin.spawn(4)
+            if event.key == pygame.K_6:
+                maguin.spawn(5)
+        inimigos.append(maguin)
         if event.type == pygame.QUIT:
             run = False
 
@@ -134,7 +216,9 @@ while run:
     unidade_mouse = carta_esqueleto.spawn_uni(event, unidade_mouse)
     for i in range(11):
         for j in range(6):
-            unidade_mouse = tabuleiro.lugar[i][j].colocar_unidade(event, unidade_mouse)
+            unidade_mouse = tabuleiro.lugar[i][j].colocar_unidade(event, unidade_mouse, inimigos, projeteis)
+
+    maguin.mover()
 
     pygame.time.delay(15)
 

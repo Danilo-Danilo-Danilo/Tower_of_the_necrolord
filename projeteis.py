ae7d1cd73fc2e95fb08101d1b2f3d965b4de63b7
Danilo-Danilo-Danilo <danilo.lacerda@aluno.ufca.edu.br>
2022-10-26 19:10:42 -0300

    consertando projeteis

---
+++ projeteis.py
@@ -0,0 +1,27 @@
+from projetil import *
+import pygame
+
+class Projeteis:
+    def __init__(self):
+        self.projeteis =[]
+
+    def ad_projetil(self, projetil):
+        self.projeteis.append(projetil)
+
+    def exibir(self, win):
+        for projetil in self.projeteis:
+            projetil.exibirp(win)
+            if projetil.x > 768:
+                 self.projeteis.remove(projetil)
+            projetil.movimento()
+
+    def colisao(self, matriz_inimigos):
+        for projetil in self.projeteis:
+            for inimigo in matriz_inimigos:
+                if (inimigo.x <= projetil.x <= inimigo.x + 32) and (inimigo.y <= projetil.y <= inimigo.y +32):
+                    if projetil in self.projeteis:
+                        self.projeteis.remove(projetil)
+                        inimigo.vida -= projetil.dano
+                        if inimigo.vida <= 0:
+                            matriz_inimigos.remove(inimigo)
+        return matriz_inimigos
